2e17e0805dc1d903240454dd5808189952a8eabc
realjesse <154276383+realjesse@users.noreply.github.com>
2025-03-20 11:09:00 -0700

    Create loop functionality

--- script/createGrid.js
+++ script/createGrid.js
@@ -2,5 +2,11 @@
 let sketchAreaNode = document.querySelector("sketch_area");
 
 function createGrid(numOfPixels) {
-    
+    let lengthOfPixel = sketchAreaNode.offsetWidth / numOfPixels;
+
+    for (i = 0; i < numOfPixels; i++) {
+        for (j = 0; j < numOfPixels, j++) {
+            
+        }
+    }
 }
