9cc45afde2da2f38944c48703aa8afa17ca824ef
realjesse <154276383+realjesse@users.noreply.github.com>
2025-03-20 11:43:55 -0700

    Add comments

--- script/createGrid.js
+++ script/createGrid.js
@@ -1,13 +1,17 @@
 // Initialize variables and constants
-let sketchAreaNode = document.querySelector("sketch_area");
+let sketchAreaNode = document.querySelector("#sketch_area");
 
+// Create the pixelated grid
 function createGrid(numOfPixels) {
+    // Calculate size of pixel
     const lengthOfPixel = sketchAreaNode.offsetWidth / numOfPixels;
 
+    // Create the row container for pixels
     for (i = 0; i < numOfPixels; i++) {
         const rowPixelContainer = document.createElement("div");
         rowPixelContainer.style.display = "flex";
 
+        // Create the individual pixels
         for (j = 0; j < numOfPixels; j++) {
             const pixel = document.createElement("div");
             pixel.style.width = `${lengthOfPixel}px`;
@@ -15,8 +19,10 @@ function createGrid(numOfPixels) {
             rowPixelContainer.appendChild(pixel);
         }
 
+        // Append to main page
         sketchAreaNode.appendChild(rowPixelContainer);
     }
 }
 
+// Test
 createGrid(16);
